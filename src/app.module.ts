b096a7fb6b448a0f0c88cf271a00143bf9c2519b
Ersapta Aristo <aristoersapta@gmail.com>
2024-02-11 22:14:17 +0700

    [main] enable public folder accessible via HTTP

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { LatihanModule } from "./latihan/latihan.module";
 import { TaskModule } from "./task/task.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
-    imports: [LatihanModule, TaskModule, PrismaModule, AuthModule],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, "..", "public"),
+        }),
+        LatihanModule,
+        TaskModule,
+        PrismaModule,
+        AuthModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
