591a34f9d15c072c5df88c9ce0d620ff7ff88272
Ersapta Aristo <aristoersapta@gmail.com>
2024-02-11 16:12:09 +0700

    [main] connecting service to controller with explanation

--- src/latihan/latihan.controller.ts
+++ src/latihan/latihan.controller.ts
@@ -1,9 +1,16 @@
 import { Controller, Get } from "@nestjs/common";
+import { LatihanService } from "./latihan.service";
 
 @Controller("latihan") // meaning we can access the route name "latihan"
 export class LatihanController {
+    constructor(
+        // LatihanService should be inside providers and should be injectable
+        // before can include here
+        private latihanService: LatihanService,
+    ) {}
+
     @Get()
     getLatihan(): string {
-        return "Latihan route";
+        return this.latihanService.panggiLatihan();
     }
 }
--- src/latihan/latihan.service.ts
+++ src/latihan/latihan.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class LatihanService {}
+export class LatihanService {
+    panggiLatihan(): string {
+        return "yuk latihan";
+    }
+}
