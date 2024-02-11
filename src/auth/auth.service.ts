6efc03a50281a52ecf5f30ce01e38a84740e5602
Ersapta Aristo <aristoersapta@gmail.com>
2024-02-11 22:47:37 +0700

    [main] include tasks list whenever get profile data

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -107,6 +107,7 @@ export class AuthService {
                 name: true,
                 email: true,
                 avatar: true,
+                tasks: true,
             },
         });
     }
